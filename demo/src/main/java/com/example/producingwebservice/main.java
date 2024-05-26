813cae3901ae8971650a70c742a5863598f2a360
monikatfy192 <93496967+monikatfy192@users.noreply.github.com>
2024-05-26 09:37:54 +0300

    Add files via upload
    secuirty project 1

---
+++ demo/src/main/java/com/example/producingwebservice/main.java
@@ -0,0 +1,11 @@
+package com.example.producingwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class main {
+    public static void main(String[] args) {
+        SpringApplication.run(main.class,args);
+    }
+}
---
+++ demo/src/test/java/com/example/producingwebservice/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.producingwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
